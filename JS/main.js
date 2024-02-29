c9271d9c050976bc05b578f4cafc36cfa38d3451
caellex <157691477+caellex@users.noreply.github.com>
2024-02-29 21:07:01 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,55 +1,51 @@
-    // Modell
-    let aCounter = 0;
-    let stringLength;
-    let getString;
+// Modell
+let aCounter = 0; // Counts vowels.
+let stringLength; // Keeps track of input string length.
+let getString; // Used in multiple places, so needed it global.
 
 
 
 
-    // View
-    updateView();
-    function updateView(){
+// View
+updateView();
+function updateView() {
     document.getElementById('app').innerHTML = /*HTML*/`
     <h1> Vowel Finder </h1>
-    <input id="inputField" oninput="getInput()" />
+    <input id="inputField" oninput="getInput()" /> <!-- See function getInput() on line 26. -->
     <button onclick="countVowels()">Count Vowels</button>
     <div id="result" class="result"><ul></ul></div>
     `
 
 
-    }
+}
 
 
 
+// Controller
+
+function getInput() {
+    getString = document.getElementById('inputField').value // Gets current input value, combined with a oninput on line 14.
+}
 
 
+function countVowels() {
+    aCounter = 0; // Makes sure that aCounter is 0 when the function is ran, so the values don't carry over from the previous execution.
+    let vowelsArray = []; // Empty array that gets filled in the if statement below.
 
-
-    // Controller
-
-    function getInput(){
-    getString = document.getElementById('inputField').value
-    }
-
-
-    function countVowels(){
-        let vowelsArray = [];
-
-    for(i=0; i <= getString.length; i++){
-        if(getString[i] === "a" || getString[i] === "e" || getString[i] === "i" || getString[i] === "o" || getString[i] === "u" || getString[i] === "y"){
+    for (i = 0; i <= getString.length; i++) { // Simple for loop to make sure it loops through the entire input
+        if (getString[i] === "a" || getString[i] === "e" || getString[i] === "i" || getString[i] === "o" || getString[i] === "u" || getString[i] === "y") { // Gets the current value index and compares it to all vowels.
             aCounter++;
             vowelsArray.push(getString[i])
-            }
+        }
     }
     vowelsFound(aCounter, vowelsArray);
-    }
+}
 
-    function vowelsFound(aCounter, vowelsArray){
-        let listItems = vowelsArray.map(vowel => `<li>${vowel}</li>`).join('')
+function vowelsFound(aCounter, vowelsArray) { // aCounter and vowelsArray are being pulled as params to make sure they are up to date. It also makes the it so 
+    let listItems = vowelsArray.map(vowel => `<li>${vowel}</li>`).join('') // .map creates a new array, giving each value the <li> tags. .join is used to choose a seperator between values in arrays. Example: "val1, val2" (", " is the seperator).
     document.getElementById('result').innerHTML = /*HTML*/`
-    Vowels Found! There are ${aCounter} vowel(s) present.<br>
-    The vowels are:
-    <ul>${listItems}</ul>
-
-    `
-    }
+        Vowels Found! There are ${aCounter} vowel(s) present.<br>
+        The vowels are:
+        <ul>${listItems}</ul> <!-- <ul> is required with <li>
+        `
+}
